70ae2d94376c5c4b19a453a03ee6468258a114ae
cat.tran03 <ngoccatt@gmail.com>
2026-05-02 16:26:58 +0700

    Revert "Merge branch 'AI_integrating' into cat_impl"
    This reverts commit 7d45c5c51193778881dadc3753d84b120ee459d8, reversing
    changes made to f52428678c6610d61010d78fb5f62d785d4a181b.

--- arctos_bringup/launch/arctos_bringup.launch.py
+++ arctos_bringup/launch/arctos_bringup.launch.py
@@ -179,6 +179,6 @@ def generate_launch_description():
         joint_state_broadcaster_spawner,
         delay_robot_arm_controller_spawner,
         delay_rviz_and_moveit_launch,
-        # camera_node,
-        # can_launch
+        camera_node1,
+        camera_node2,
     ])
